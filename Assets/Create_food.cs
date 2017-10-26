2ebfd784bf32732cf534400b34df62a2f8f59e87
TwistWind <tw.twistwind@gmail.com>
2017-10-26 18:33:52 +0900

    1.0
    Base structure.

---
+++ Assets/Create_food.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Create_food : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject food;
+
+    public Transform border_Top;
+    public Transform border_Down;
+    public Transform border_Left;
+    public Transform border_Right;
+
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("Create", 3, 5);
+    }
+    void Create()
+    {
+        int x = (int)Random.Range(border_Left.position.x,
+                                  border_Right.position.x);
+
+        int y = (int)Random.Range(border_Down.position.y,
+                                  border_Top.position.y);
+
+        Instantiate(food,
+                    new Vector2(x, y),
+                    Quaternion.identity); // default rotation
+    }
+}
---
+++ Assets/snake.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class snake : MonoBehaviour {
+
+    bool ate = false;
+    public GameObject snake_tail;
+    Vector2 dir = Vector2.right;
+    List<Transform> tail = new List<Transform>();
+
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("Movement", 0.5f, 0.5f);
+    }
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        // Food?
+        if (coll.name.StartsWith("food"))
+        {
+            // Get longer in next Move call
+            ate = true;
+
+            // Remove the Food
+            Destroy(coll.gameObject);
+        }
+        // Collided with Tail or Border
+    }
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey(KeyCode.RightArrow))
+            dir = Vector2.right;
+        else if (Input.GetKey(KeyCode.DownArrow))
+            dir = -Vector2.up;    // '-up' means 'down'
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            dir = -Vector2.right; // '-right' means 'left'
+        else if (Input.GetKey(KeyCode.UpArrow))
+            dir = Vector2.up;
+    }
+    void Movement()
+    {
+        Vector2 v = transform.position;
+        transform.Translate(dir);
+        if (ate)
+        {
+            // Load Prefab into the world
+            GameObject g = (GameObject)Instantiate(snake_tail,v,Quaternion.identity);
+
+            // Keep track of it in our tail list
+            tail.Insert(0, g.transform);
+
+            // Reset the flag
+            ate = false;
+        }
+        // Do we have a Tail?
+        else if (tail.Count > 0)
+        {
+            // Move last Tail Element to where the Head was
+            tail.Last().position = v;
+
+            // Add to front of list, remove from the back
+            tail.Insert(0, tail.Last());
+            tail.RemoveAt(tail.Count - 1);
+        }
+    }
+}
